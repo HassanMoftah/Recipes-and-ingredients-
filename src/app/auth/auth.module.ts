17d4dd143aa04ae8178c131d6bfe11a2cb4caa16
moftah <hassanosamamoftah@gmail.com>
2020-02-02 02:05:15 +0200

    second commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { AuthComponent } from './auth.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+    declarations:[
+        AuthComponent
+    ],
+    imports:[FormsModule,CommonModule,SharedModule,RouterModule.forChild([ { path: '', component: AuthComponent }])],
+    exports:[AuthComponent]
+})
+export class AuthModule{
+
+}
